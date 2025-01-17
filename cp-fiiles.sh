f622aacbbacd1f67337b0eed9dac75ddb7587573
heejin131 <journeyjean131@gmail.com>
2025-01-17 16:35:06 +0900

    docs del

--- cp-fiiles.sh
+++ cp-fiiles.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
+pwd
+date
+timedatectl
+cp prd-build.sh
 pnpm docs:build
 cp CNAME docs/
 cp robots.txt docs/
 cp naverf9541c054d6beab319f39bc6d207e2d8.html docs/
-git add .
-git commit -m "build"
-git push
+cat /etc/os-release
